b3593466a1d211f7bda73a1da5122da9805c56c8
Alexandre Lemes <alexandreeduardolemes@gmail.com>
2023-08-09 20:04:41 -0300

    feat: gera crud de user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
